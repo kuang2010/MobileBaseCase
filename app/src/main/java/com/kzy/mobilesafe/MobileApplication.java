2d9c50ccc293cac4abf833156922ed5b34083b3e
kuangzeyu2019 <gdf>
2020-07-12 17:12:43 +0800

    splash

--- app/src/main/java/com/kzy/mobilesafe/MobileApplication.java
+++ app/src/main/java/com/kzy/mobilesafe/MobileApplication.java
@@ -63,7 +63,7 @@ public class MobileApplication extends Application {
 
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
-        for (int i=0;i<50;i++){
+        for (int i=0;i<100;i++){
             //模拟延时启动白屏
             SystemClock.sleep(100);
         }
